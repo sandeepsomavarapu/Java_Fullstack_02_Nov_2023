ecad113a1299dd85a3b7f184420ee80580dbec00
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-11-06 21:31:11 +0530

    Day-3 Updates

---
+++ Variables_Ex.java
@@ -0,0 +1,15 @@
+class Variables_Ex
+{
+	public static void main(String args[])
+	{
+		char gender='M';
+		byte age=127;
+		long contact=9494348734l;
+		float salary=23.54235476932243f;
+		double result=23.54235476932243;
+		boolean value=true;
+		System.out.println("welcome to java");	
+		System.out.println(gender+" "+age+" "+salary+"  "+contact+"  "+result);	
+	}
+	
+}
